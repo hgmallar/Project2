e7c664a21067ffd323f30e70e45ac9fcbb5ef4b1
EAWehner <elizabethawehner@gmail.com>
2018-12-20 20:17:32 -0500

    updated users model with more columns

---
+++ models/users.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    loggedOn: DataTypes.BOOLEAN,
+    wins: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    losses: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  });
+  return User;
+};
